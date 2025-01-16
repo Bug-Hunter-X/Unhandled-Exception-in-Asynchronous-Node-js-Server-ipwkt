00d294a3a1850ff92deb008a2a102d5e54723f79
BugHunter-X <askzenai@gmail.com>
2025-01-16 02:04:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  // Simulate an asynchronous operation that might fail
+  setTimeout(() => {
+    if (Math.random() < 0.5) {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Success!');
+    } else {
+      // Incorrect way to handle the error, will crash the server
+      throw new Error('Simulated error');
+    }
+  }, 100);
+});
+
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
